f71539133bf9a33fb39e8f83cf83560123395666
Anthony Pardo <pardo.anthony@gmail.com>
2019-10-25 23:35:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   { railroads: {pieces: 4, rent_in_dollars: {names: 2, hello: 2, wat: 2, now: 2},
-    names: 4
+    names: {names: 2, hello: 2, wat: 2, now: 2}
   }}
 end
 
